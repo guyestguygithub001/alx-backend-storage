e34a12e7ae4f3215cdcb8f8c8338b19157363033
guyestguygithub001 <danielene050@gmail.com>
2024-05-09 11:53:58 +0100

    changed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- This query retrieves all bands identified with Glam rock as their primary genre and ranks them by their 'lifespan'. The 'lifespan' is calculated as the difference between the year they split and the year they were formed, using '2020' as the default split year if the actual split year is not available.
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- A SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
