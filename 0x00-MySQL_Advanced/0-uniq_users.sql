6844731cd99722ee954526d625aab7ddd1e2315a
guyestguygithub001 <danielene050@gmail.com>
2024-05-09 11:07:51 +0100

    no mistakes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,7 @@
--- Check for the existence of the 'users' table and remove it if it exists
-IF OBJECT_ID('users', 'U') IS NOT NULL
-    DROP TABLE users;
-
--- Creation of the 'users' table with unique email addresses
+-- This statement removes the 'users' table if it already exists, then creates a new 'users' table with a primary key 'id', a unique 'email', and a 'name' field.
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    email NVARCHAR(255) NOT NULL,
-    name NVARCHAR(255),
-    CONSTRAINT UC_Email UNIQUE (email)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
